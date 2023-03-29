3a9bebbcc1ad84023a4b25f6bb8e90ac4ba6a264
KetulPatel14 <akhilroy877@gmail.com>
2023-03-29 10:28:55 -0400

    Modified design

---
+++ IosCodingNavigator/IosCodingNavigator/InfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  InfoViewController.swift
+//  IosCodingNavigator
+//
+//  Created by Canadore Student on 2023-03-29.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var Pw: UILabel!
+    @IBOutlet var Pcode: UILabel!
+    @IBOutlet weak var City: UILabel!
+    @IBOutlet var StreetAdress: UILabel!
+    @IBOutlet weak var Email: UILabel!
+    @IBOutlet weak var LName: UILabel!
+    @IBOutlet weak var fName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
